a175a606415e4684d5af39c6bb98ef7d0f0a87e7
Antenor <antenorzapata@gmail.com>
2021-08-27 17:08:26 -0300

    feat: create server

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+process.on('uncaughException', (err) => {
+  console.log('UNCAUGHT EXCEPTION: Shuting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('db connection success');
+  });
+
+const port = process.env.PORT;
+const server = app.listen(port, () => {
+  console.log('app rodando na porta 3001');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('unhandler rejection Shuting down');
+  server.close(() => {
+    process.exit(1);
+  });
+});
--- server/src/presentation/controllers/signup.ts
+++
@@ -1,5 +0,0 @@
-export class SignupController {
-  handle (httpRequest: any): any {
-    return { statusCode: 400 }
-  }
-}
