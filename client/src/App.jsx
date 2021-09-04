34ad0258f2572bf5235106172713eb17c7e0e202
Antenor <antenorzapata@gmail.com>
2021-09-04 18:19:14 -0300

    feat: add user register, resetPassword and forgotPassword pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 import GuitarCard from './components/guitarCard/GuitarCard';
 import Form from './components/form/Form';
@@ -19,6 +22,10 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/forgotPassword" component={ForgotPassword} />
+        <Route exact path="/resetPassword" component={ResetPassword} />
+
       </Switch>
       {/* <GuitarCard />
       <Form /> */}
