94fb7533bd26ef5328e95539cd3ae2cff4105e31
Antenor <antenorzapata@gmail.com>
2021-09-07 22:34:29 -0300

    feat: starting login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const [state, setState] = useState({ email: '', password: '' });
+
+  const hendleSubmit = () => {
+
+  };
+
   return (
     <div>
-      <h1>login</h1>
+      <form>
+        <label htmlFor="email">
+          Email
+          <input type="email" required id="email" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="text" />
+        </label>
+        <button type="submit">Entrar</button>
+        <Link to="/forgotPassword">Esqueci minha senha</Link>
+      </form>
     </div>
   );
 }
