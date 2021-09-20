c274493809de33869ee0f5b217426a78f751697b
Antenor Zapata <antenorzapata@gmail.com>
2021-09-19 23:51:22 -0300

    feat: add deleteFavorite endpoint

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -26,7 +26,14 @@ const getAll = catchAsync(async (req, res) => {
   return res.status(200).json({ status: 'success', favorites });
 });
 
+const deleteFavorite = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const response = await Favorite.findByIdAndDelete(id);
+  return res.status(200).json({ status: 'success', response });
+});
+
 module.exports = {
   create,
   getAll,
+  deleteFavorite,
 };
--- server/routes/favoritesRouter.js
+++ server/routes/favoritesRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { create, getAll, deleteReview } = require('../controllers/favoritesController');
+const { create, getAll, deleteFavorite } = require('../controllers/favoritesController');
 
 const { validateToken } = require('../middlewares/auth');
 
@@ -7,8 +7,8 @@ const { validateId } = require('../middlewares/validateGuitar');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getAll).post(validateToken, create);
+router.route('/').get(validateToken, getAll).post(validateToken, create);
 
-// router.route('/:id').delete(validateToken, validateId, deleteReview);
+router.route('/:id').delete(validateToken, validateId, deleteFavorite);
 
 module.exports = router;
