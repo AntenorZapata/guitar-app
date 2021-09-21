9c6e74052c981243f3ef06e71940586931f5b7a4
Antenor Zapata <antenorzapata@gmail.com>
2021-09-21 02:35:47 -0300

    feat: add delete favorite feature to favorite Page

--- client/src/components/guitarCard/GuitarCard.jsx
+++ client/src/components/guitarCard/GuitarCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 
-function GuitarCard({ guitar, favorite }) {
+function GuitarCard({ guitar, favorite, handleDeleteFav }) {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem('token') || '';
+
   return (
     <div>
-
       {favorite ? (
         <div className="favorite-card">
+          <MdFavorite onClick={() => handleDeleteFav(guitar._id)} />
           <p>{guitar.brand}</p>
           <p>{guitar.model}</p>
           <p>{guitar.year}</p>
--- client/src/pages/Favorites.jsx
+++ client/src/pages/Favorites.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
+// import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../components/header/Header';
 import GuitarCard from '../components/guitarCard/GuitarCard';
 import {
-  getGuitarByIdAction, getReviews, getReviewById, createReviewAction,
-  deleteReviewAction, createFavoriteAction, deleteFavoriteAction,
-  getFavoriteByEmailAction, getGuitars,
+  // getGuitarByIdAction, getReviews, getReviewById, createReviewAction,
+  // deleteReviewAction, createFavoriteAction, deleteFavoriteAction,
+  getFavoriteByEmailAction, deleteFavoriteAction,
 } from '../actions';
-import { getGuitarById } from '../api';
+// import { getGuitarById } from '../api';
 
 function Favorites() {
   const token = localStorage.getItem('token') || '';
@@ -16,8 +16,8 @@ function Favorites() {
   let email = '';
   if (userLocal) email = userLocal.email;
   const favorites = useSelector((state) => state.favorites.allFavorites);
-  const guitars = useSelector((state) => state.guitars.allGuitars);
-  const favoritesPage = useSelector((state) => state.favorites.favoritesPage);
+  // const guitars = useSelector((state) => state.guitars.allGuitars);
+  // const favoritesPage = useSelector((state) => state.favorites.favoritesPage);
 
   const dispatch = useDispatch();
 
@@ -25,14 +25,20 @@ function Favorites() {
     if (token) {
       dispatch(getFavoriteByEmailAction(email, token));
     }
-  }, []);
+  }, [favorites]);
+
+  const handleDeleteFav = async (id) => {
+    await dispatch(deleteFavoriteAction(id, token));
+  };
+
+  console.log('de novo');
 
   return (
     <div>
       <Header />
       <h1>favorite</h1>
       {favorites.length ? favorites.map((gt) => (
-        <GuitarCard guitar={gt} favorite key={gt.guitar} />
+        <GuitarCard guitar={gt} favorite key={gt.guitar} handleDeleteFav={handleDeleteFav} />
       )) : null}
     </div>
   );
